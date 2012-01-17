a2ae4af7c78e6ed4c05b38aaeb576b3d2925509a
madx <madx@yapok.org>
2012-01-17 19:01:20 +0100

    make plugin setups cleaner

--- lib/roy/halt.rb
+++ lib/roy/halt.rb
@@ -2,10 +2,12 @@ module Roy
   module Halt
 
     def self.setup(roy)
-      class << roy
-        def halt(code, message=nil)
-          throw :halt, [code, message || Rack::Utils::HTTP_STATUS_CODES[code]]
-        end
+      roy.send(:extend, InstanceMethods)
+    end
+
+    module InstanceMethods
+      def halt(code, message=nil)
+        throw :halt, [code, message || Rack::Utils::HTTP_STATUS_CODES[code]]
       end
     end
 
--- lib/roy/render.rb
+++ lib/roy/render.rb
@@ -4,20 +4,22 @@ module Roy
   module Render
 
     def self.setup(roy)
-      class << roy
-        def render(engine, view_or_string, params={}, &block)
-          options = conf.render || {}
-          template = case view_or_string
-            when Symbol
-              file = [view_or_string.to_s, engine].map(&:to_s).join('.')
-              dir = conf.views || 'views'
-              Tilt.new(File.join(dir, file), nil, options)
-            else
-              Tilt[engine].new(nil, nil, options) { view_or_string.to_s }
-            end
+      roy.send(:extend, InstanceMethods)
+    end
 
-          template.render(app, params, &block)
-        end
+    module InstanceMethods
+      def render(engine, view_or_string, params={}, &block)
+        options = conf.render || {}
+        template = case view_or_string
+          when Symbol
+            file = [view_or_string.to_s, engine].map(&:to_s).join('.')
+            dir = conf.views || 'views'
+            Tilt.new(File.join(dir, file), nil, options)
+          else
+            Tilt[engine].new(nil, nil, options) { view_or_string.to_s }
+          end
+
+        template.render(app, params, &block)
       end
     end
 
